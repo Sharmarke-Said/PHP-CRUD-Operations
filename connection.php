c1beceda37c4482b2c1b243da417b7c2a2af4c36
Sharmarke Said <sharmakesaid17@gmail.com@gmail.com>
2023-10-18 13:40:46 +0300

    Updated the Database connection

--- connection.php
+++ connection.php
@@ -1,9 +1,20 @@
 <?php
+// Define the database parameters
+$servername = "localhost"; // The name of the server where the database is hosted
+$username = "root"; // The username for accessing the database
+$password = ""; // The password for accessing the database
+$dbname = "studentsdb"; // The name of the database
 
-$con = new mysqli('localhost', 'root', '', 'usersdb');
+// Create a new connection object using the mysqli class
+$con = new mysqli($servername, $username, $password, $dbname);
 
-if(!$con){
-    die(mysqli_error($con));
+// Check if the connection was successful or not
+if ($con->connect_error) {
+  // If there was an error, display a message and exit the script
+  die("Connection failed: " . $con->connect_error);
 }
 
+
+// Close the connection when you are done
+// $con->close();
 ?>
